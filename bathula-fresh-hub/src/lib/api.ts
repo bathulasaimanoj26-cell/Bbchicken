6e7b28a242ec44bdad6a02f5aee983d7c6a1bb0d
manitejabayyaa <maniteja.bayya369@gmail.com>
2025-09-26 19:34:47 +0530

    updated the base url

--- bathula-fresh-hub/src/lib/api.ts
+++ bathula-fresh-hub/src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance with base URL
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Update this if your backend is running on a different port
+  baseURL: 'https://bbchicken.onrender.com/api', // Updated to point to the deployed backend
   withCredentials: true, // This is important for sending cookies
   headers: {
     'Content-Type': 'application/json',
